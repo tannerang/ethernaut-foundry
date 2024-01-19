f15d20c5dfbcbc508c13b1af211820bed2e8341f
Tanner Ang <ang713943@gmail.com>
2024-01-20 04:13:13 +0800

    add notes in 17-Recovery

--- script/setup/IEthernaut.sol
+++ script/setup/IEthernaut.sol
@@ -2,6 +2,6 @@
 pragma solidity 0.8.21;
 
 interface IEthernaut {
-    function createLevelInstance(address) external payable;
+    function createLevelInstance(address) external payable; // Add `payable` due to 17-Recovery requirement.
     function submitLevelInstance(address payable) external;
 }
