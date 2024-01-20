1bb770244a6336a5c8337831e1343ee0c8cca59f
Tanner Ang <ang713943@gmail.com>
2024-01-21 04:07:43 +0800

    complete 28-GatekeeperThree

--- script/solutions/28-GatekeeperThree.s.sol
+++ script/solutions/28-GatekeeperThree.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import "../../src/28-GatekeeperThreeAttacker.sol";
 
 contract GatekeeperThreeSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x653239b3b3E67BC0ec1Df7835DA2d38761FfD882;
@@ -16,8 +17,8 @@ contract GatekeeperThreeSolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-
+        GatekeeperThreeAttacker gatekeeperThreeAttacker = new GatekeeperThreeAttacker{value: 0.0011 ether}(challengeInstance);
+        gatekeeperThreeAttacker.attack();
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
