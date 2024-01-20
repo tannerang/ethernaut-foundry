803276f0bc26b7a080b3e7ac8ba3a3afa35b0c21
Tanner Ang <ang713943@gmail.com>
2024-01-21 03:05:09 +0800

    complete 19-AlienCodex

--- script/solutions/19-AlienCodex.s.sol
+++ script/solutions/19-AlienCodex.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import "../../src/19-AlienCodexAttacker.sol";
 
 contract AlienCodexSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x0BC04aa6aaC163A6B3667636D798FA053D43BD11;
@@ -16,8 +17,8 @@ contract AlienCodexSolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-
+        AlienCodexAttacker alienCodexAttacker = new AlienCodexAttacker(challengeInstance);
+        alienCodexAttacker.attack();
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
