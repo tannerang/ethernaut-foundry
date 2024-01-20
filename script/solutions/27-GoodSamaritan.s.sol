20324be7928e58d04bd8f914e6432fc0ec1fe92f
Tanner Ang <ang713943@gmail.com>
2024-01-21 04:55:37 +0800

    complete 27-GoodSamaritan

--- script/solutions/27-GoodSamaritan.s.sol
+++ script/solutions/27-GoodSamaritan.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import "../../src/27-GoodSamaritanAttacker.sol";
 
 contract GoodSamaritanSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x36E92B2751F260D6a4749d7CA58247E7f8198284;
@@ -16,8 +17,8 @@ contract GoodSamaritanSolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-
+        GoodSamaritanAttacker goodSamaritanAttacker = new GoodSamaritanAttacker(challengeInstance);
+        goodSamaritanAttacker.attack();
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
