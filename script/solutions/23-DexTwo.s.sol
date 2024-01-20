ae421d2c59c9ca496fad9bc77283dd29eed4a125
Tanner Ang <ang713943@gmail.com>
2024-01-20 08:08:20 +0800

    complete 23-DexTwo

--- script/solutions/23-DexTwo.s.sol
+++ script/solutions/23-DexTwo.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import "../../src/23-DexTwoAttacker.sol";
 
 contract DexTwoSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0xf59112032D54862E199626F55cFad4F8a3b0Fce9;
@@ -13,11 +14,11 @@ contract DexTwoSolution is Script, EthernautHelper {
     function run() public {
         vm.startBroadcast(heroPrivateKey);
         // NOTE this is the address of your challenge contract
-        address challengeInstance = createInstance(LEVEL_ADDRESS);
+        address challengeInstance = _createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-
+        DexTwoAttacker dexTwoAttacker = new DexTwoAttacker{value: 400}(challengeInstance);
+        dexTwoAttacker.attack();
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
