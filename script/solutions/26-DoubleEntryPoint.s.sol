9c626e5d88b258506c1503995c9f4565a7d0c7b6
Tanner Ang <ang713943@gmail.com>
2024-01-22 02:01:53 +0800

    complete 26-DoubleEntryPoint

--- script/solutions/26-DoubleEntryPoint.s.sol
+++ script/solutions/26-DoubleEntryPoint.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import "../../src/26-DoubleEntryPointAttacker.sol";
 
 contract DoubleEntryPointSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x34bD06F195756635a10A7018568E033bC15F3FB5;
@@ -13,11 +14,12 @@ contract DoubleEntryPointSolution is Script, EthernautHelper {
     function run() public {
         vm.startBroadcast(heroPrivateKey);
         // NOTE this is the address of your challenge contract
-        address challengeInstance = createInstance(LEVEL_ADDRESS);
+        address challengeInstance = _createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-
+        DoubleEntryPointAttacker doubleEntryPointAttacker = new DoubleEntryPointAttacker(challengeInstance);
+        address forta = address(IDoubleEntryPoint(challengeInstance).forta());
+        IForta(forta).setDetectionBot(address(doubleEntryPointAttacker));
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
