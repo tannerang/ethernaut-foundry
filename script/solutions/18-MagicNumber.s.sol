ae25edbeaf66de42cd53febb8b7d376460f510cd
Tanner Ang <ang713943@gmail.com>
2024-01-23 20:27:09 +0800

    complete 18-MagicNumber

--- script/solutions/18-MagicNumber.s.sol
+++ script/solutions/18-MagicNumber.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import "../../src/18-MagicNumberAttacker.sol";
 
 contract MagicNumberSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x2132C7bc11De7A90B87375f282d36100a29f97a9;
@@ -16,8 +17,8 @@ contract MagicNumberSolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-
+        MagicNumberAttacker magicNumberAttacker = new MagicNumberAttacker(challengeInstance);
+        magicNumberAttacker.attack();
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
