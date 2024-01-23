73993b6ff51b3f33f8f609558ed690178f5ca61f
Tanner Ang <ang713943@gmail.com>
2024-01-23 09:48:25 +0800

    complete 29-Switch

--- script/solutions/29-Switch.s.sol
+++ script/solutions/29-Switch.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+import "../../src/29-SwitchAttacker.sol";
 
 contract SwitchSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0xb2aBa0e156C905a9FAEc24805a009d99193E3E53;
@@ -16,8 +17,8 @@ contract SwitchSolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-
+        SwitchAttacker switchAttacker = new SwitchAttacker(challengeInstance);
+        switchAttacker.attack();
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
